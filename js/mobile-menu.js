04ebab5a9b215a0e429097f0d346d587807df691
DominikaJazwicka <109682344+DominikaJazwicka@users.noreply.github.com>
2022-12-22 22:28:47 +0100

    main

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,33 +1,16 @@
 
 
-// const mobileNav = document.querySelector('ul');
-// const burgerIcon = document.querySelector('.burger');
+const mobileNav = document.querySelector('ul');
+const burgerIcon = document.querySelector('.burger');
 
 
 
-// burgerIcon.addEventListener('click', function(){
-//     mobileNav.classList.toggle('active');
-//     burgerIcon.classList.toggle('active'); 
+burgerIcon.addEventListener('click', function(){
+    mobileNav.classList.toggle('active');
+    burgerIcon.classList.toggle('active'); 
    
-// })
+})
 
 
-(() => {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const openMenuBtn = document.querySelector('.open-menu');
-    const closeMenuBtn = document.querySelector('.close-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  })();
-  
+
 
